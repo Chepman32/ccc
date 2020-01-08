260859f91b7f206c836ef901f96325ec617b78e6
Anton Chepur <antonkerch555@gmail.com>
2020-01-08 11:17:30 +0300

    Local

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,18 +1,35 @@
 import React, {useState} from "react";
+import PropTypes from "prop-types";
+
+function useInputValue(defaultValue = " ") {
+    const [value, setValue] = useState(defaultValue);
+    return {
+        bind: {
+            value,
+        onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(" "),
+        value: () => value
+    }
+}
 function AddForm({onCreate}) {
-    const [value, setValue] = useState(" ");
+    const input = useInputValue(" ")
     function submitHandler(event) {
-        if(value.trim()) {
-            onCreate(value);
-            setValue(" ")
+        if(input.value().trim()) {
+            onCreate(input.value());
+            input.clear()
+
             event.preventDefault()
         }
     }
     return (
         <form style={{marginBottom: "1rem"}} onSubmit={submitHandler}>
-            <input value={value} type="text" onChange={event => setValue(event.target.value)}/>
+            <input {...input.bind}/>
             <button>Add todo</button>
         </form>
     )
 };
+AddForm.propTypes = {
+    onCreate: PropTypes.func.isRequired
+}
 export default AddForm;
