b46e6aebd638058cfd8ccafa7457416fc4ab1dbf
Anton Chepur <antonkerch555@gmail.com>
2020-01-08 18:53:35 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './Todo/TodoList';
 import Context from "./Context"
 import AddForm from './AddForm';
+import Loader from './Loader';
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    {id: 1, completed: false, title: "Купить хлеб"},
-    {id: 2, completed: true, title: "Купить масло"},
-    {id: 3, completed: false, title: "Купить молоко"},
-  ])
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  .then(response => response.json())
+  .then(todos =>
+    setTimeout(()=> {setTodos(todos)
+    setLoading(false)}, 2000)
+     )
+  }, [])
   function toggleTodo(id) {
     setTodos(todos.map(todo => {
       if(todo.id === id) {
         todo.completed = !todo.completed;
       }
-      console.log("todo", todo)
       return todo;
     })
     )
@@ -22,7 +27,8 @@ function App() {
   function addTodo(title) {
     setTodos(todos.concat(
       [
-      {title,
+      {
+        title,
         id: Date.now(),
         completed: false
     }
@@ -35,7 +41,8 @@ function App() {
   return <Context.Provider value={{removeTodo}}>
       <div className="wrapper">
         <AddForm onCreate={addTodo}/>
-      {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos</p>}
+        {loading && <Loader/>}
+      {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : (loading ? null : <p>No todos</p>)}
     </div>
     </Context.Provider>
 }
